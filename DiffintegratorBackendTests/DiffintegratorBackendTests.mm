b89ceaefd2e7a0f8622da601709aa87f04af7a12
Ivan Post <ivan.post24@gmail.com>
2024-04-03 09:04:33 -0600

    Switch testing framework to XCTest

---
+++ DiffintegratorBackendTests/DiffintegratorBackendTests.mm
@@ -0,0 +1,36 @@
+//
+//  DiffintegratorBackendTests.m
+//  DiffintegratorBackendTests
+//
+//  Created by Ivan on 4/3/24.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DiffintegratorBackendTests : XCTestCase
+
+@end
+
+@implementation DiffintegratorBackendTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
